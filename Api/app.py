f6f80596984ea71f74976db39c941dd3f8f97596
Derek Miller <dmmiller612@gmail.com>
2014-03-19 14:23:29 -0400

    added form inserts

--- Api/app.py
+++ Api/app.py
@@ -85,8 +85,55 @@ class Registration(Resource):
         else:
             return {'success': 'False', 'message': 'You have illegal characters in your username'}
 
+class FormInsert(Resource):
+
+    def __init(self):
+
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('unique_id', type=str, location='form')
+        self.parser.add_argument('name', type=str, location='form')
+        self.parser.add_argument('food', type=str, location='form')
+        self.parser.add_argument('music', type=str, location='form')
+        self.parser.add_argument('movie', type=str, location='form')
+        self.parser.add_argument('book', type=str, location='form')
+        self.parser.add_argument('poem', type=str, location='form')
+        self.parser.add_argument('quote', type=str, location='form')
+        self.args = self.parser.parse_args()
+
+    def post(self):
+        insert_data = self.insert_data()
+        return insert_data
+
+    def insert_data(self):
+
+        conn = mdb.connect('localhost', 'root', 'password', 'api', cursorclass=MySQLdb.cursors.DictCursor)
+        with conn:
+            cur = conn.cursor()
+            select_query = "SELECT full_name, food, music, movie, book, poem, quote FROM form WHERE unique_id = '%s'" \
+                           % self.args['unique_id']
+            cur.execute(select_query)
+
+            if cur.rowcount == 0:
+                insert_query = """
+                               INSERT INTO form (unique_id, full_name, food, music, movie, book, poem, quote) VALUES
+                               ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
+                               """ % (self.args['unique_id'], self.args['name'], self.args['food'], self.args['music'],
+                               self.args['movie'], self.args['book'], self.args['poem'], self.args['quote'])
+                cur.execute(insert_query)
+                return {'success': 'True'}
+            else:
+                update_query = """
+                               UPDATE form SET full_name='%ss', food='%s', music='%s', movie='%s', book='%s', poem='%s',
+                                quote='%s' WHERE unique_id = '%s')
+                               """ % (self.args['name'], self.args['food'], self.args['music'], self.args['movie'],
+                                      self.args['book'], self.args['poem'], self.args['quote'], self.args['unique_id'])
+                cur.execute(update_query)
+                return {'success': 'True'}
+
+
 api.add_resource(DataChecker, '/login/')
 api.add_resource(Registration, '/register/')
+api.add_resource(FormInsert, '/insertform/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='192.168.56.101')
